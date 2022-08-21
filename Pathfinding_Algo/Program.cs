cbe151d3404525ceb65ab833615ce26237ae6180
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-20 22:43:34 -0700

    initial commit, 30FPS

---
+++ Pathfinding_Algo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pathfinding_Algo
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            int lastTick = 0;
+            const int WAIT_TICK = 1000 / 30;
+
+            while (true)
+            {
+                int currentTick = System.Environment.TickCount;
+                int elapsedTick = currentTick - lastTick;
+
+                // if elapsedTick is less than 1/30 seconds
+                if (elapsedTick < 1000 / 30)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                // user input
+
+                // logic
+
+                // render
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Hello World!");
+            }
+
+        }
+    }
+}
+
