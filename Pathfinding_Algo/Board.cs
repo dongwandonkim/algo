1ee95857e6f791d94e2a28dd4794bd6c4982ca99
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-21 17:56:01 -0700

    dynamic list

---
+++ Pathfinding_Algo/Board.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pathfinding_Algo
+{
+    public class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+
+        public int Count = 0; // real usage data
+        public int Capacity { get { return _data.Length; } }// reserved data
+
+        public void Add(T item)
+        {
+            // check if enough room available
+            if (Count >= Capacity)
+            {
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    newArray[i] = _data[i];
+                }
+                _data = newArray;
+            }
+
+            //
+            _data[Count] = item;
+            Count++;
+        }
+
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Count - 1] = default(T);
+
+            Count--;
+        }
+    }
+
+    public class Board
+    {
+        public int[] _data = new int[25];
+        public MyList<int> _data2 = new MyList<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+
+        public Board()
+        {
+        }
+
+        public void ListInitialize()
+        {
+            _data2.Add(101);
+            _data2.Add(102);
+            _data2.Add(103);
+            _data2.Add(104);
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
+        }
+    }
+}
+
--- Pathfinding_Algo/Program.cs
+++ Pathfinding_Algo/Program.cs
@@ -8,29 +8,43 @@ namespace Pathfinding_Algo
 
         static void Main(string[] args)
         {
+            Board board = new Board();
+            board.ListInitialize();
+
             Console.CursorVisible = false;
 
             int lastTick = 0;
-            const int WAIT_TICK = 1000 / 30;
-
+            const int WAIT_TICK = 1000 / 30; // 1/30 seconds - 30fps
+            const char CIRCLE = '\u25cf';
             while (true)
             {
+                #region FPS
                 int currentTick = System.Environment.TickCount;
                 int elapsedTick = currentTick - lastTick;
 
                 // if elapsedTick is less than 1/30 seconds
-                if (elapsedTick < 1000 / 30)
+                if (elapsedTick < WAIT_TICK)
                 {
                     continue;
                 }
                 lastTick = currentTick;
+                #endregion
                 // user input
 
                 // logic
 
                 // render
                 Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Hello World!");
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int y = 0; y < 25; y++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
             }
 
         }
