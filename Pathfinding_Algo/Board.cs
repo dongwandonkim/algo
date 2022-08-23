0c246c88f482da66aac2a1fd8b7c37faa5120455
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-22 18:12:29 -0700

    side winder

--- Pathfinding_Algo/Board.cs
+++ Pathfinding_Algo/Board.cs
@@ -27,7 +27,68 @@ namespace Pathfinding_Algo
             _size = size;
             //
 
-            GenerateBinaryTreeMaze();
+            //GenerateBinaryTreeMaze();
+            GenerateSideWinderMaze();
+
+        }
+        public void GenerateSideWinderMaze()
+        {
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        _tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            // create path
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                int count = 0;
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        continue;
+                    }
+
+                    if (y == _size - 2 && x == _size - 2) continue;
+
+                    // second last
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    // create path 50% right or bottom
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        // go back 2 indexes where it was TileType.Emply initialy
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 0;
+                    }
+                }
+            }
 
         }
 
@@ -73,6 +134,7 @@ namespace Pathfinding_Algo
                         continue;
                     }
 
+                    // create path 50% right or bottom
                     if (rand.Next(0, 2) == 0)
                     {
                         _tile[y, x + 1] = TileType.Empty;
